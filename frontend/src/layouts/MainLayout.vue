b446a839d5c5df1d64a8eb49da03bd25cff15e93
XAEA2112 <llanas.jm@hotmail.com>
2024-08-15 14:31:30 -0600

    Se añadio un nav para la homepage

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -1,9 +1,19 @@
 <script lang="ts" setup>
-import { IonRouterOutlet, IonContent } from "@ionic/vue";
+import { defineComponent, DefineComponent } from "vue";
+import { IonRouterOutlet, IonContent, IonPage } from "@ionic/vue";
+
+
 </script>
 <template>
+  <IonPage>
   <div class="w-full h-16 bg-primary"></div>
+  <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/register">Register</RouterLink>
+      <RouterLink to="/login">Login</RouterLink>
+  </nav>
   <IonContent>
     <IonRouterOutlet />
   </IonContent>
+</IonPage>
 </template>
