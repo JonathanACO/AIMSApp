4f9b468a9576ef1b807f0e43c8fde849c2ccbeb6
XAEA2112 <llanas.jm@hotmail.com>
2024-08-15 14:32:51 -0600

    se definieron las rutas para el front

--- frontend/src/router/routes.ts
+++ frontend/src/router/routes.ts
@@ -1,12 +1,22 @@
+import { Component } from "ionicons/dist/types/stencil-public-runtime";
+
 const routes = [
   {
     path: "/login",
     name: "login",
+    meta: { requireAuth: false },
     component: () => import("../pages/Login/LoginPage.vue"),
+  },
+  {
+    path: "/register",
+    name: "register",
+    meta: { requireAuth: false },
+    component: () => import("../pages/Register/RegisterPage.vue")
   },
   {
     path: "/",
     name: "main-layout",
+    meta: { requireAuth: true },
     component: () => import("../layouts/MainLayout.vue"),
     children: [
       {
