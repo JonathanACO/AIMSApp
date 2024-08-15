3c8df285be592c145eacd744fe91558308942f42
XAEA2112 <llanas.jm@hotmail.com>
2024-08-15 14:22:24 -0600

    Se montó la página del login

--- frontend/src/pages/Home/HomePage.vue
+++ frontend/src/pages/Home/HomePage.vue
@@ -1,4 +1,47 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import { useAuthStore } from '@/stores/auth';
+import { IonContent } from '@ionic/vue';
+import { IonPage } from '@ionic/vue';
+import { useRouter } from 'vue-router'
+import { User } from '@/entities/user';
+
+const auth = useAuthStore()
+const router = useRouter()
+const user = ref<User>()
+const loading = ref(true)
+
+async function logout() {
+  await auth.logout()
+  router.push({ name: 'login' })
+}
+
+async function getUser(){
+  const response = await auth.me()
+  if(!response) return;
+  user.value = response
+  }
+
+onMounted(async () => {
+ await getUser()
+})
+</script>
+
 <template>
-  <div></div>
+  <IonPage>
+    <IonContent>
+      <div v-if="user">
+        <span>
+          Welcome. {{ user.email  }}!
+        </span>
+
+        <form @submit.prevent="logout">
+          <button type="submit">Logout</button>
+        </form>
+      </div>
+      <div v-else>
+
+      </div>
+    </IonContent>
+  </IonPage>
 </template>
