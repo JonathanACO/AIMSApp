312d99fe6cf651e2aaeb5a3c6bd73a4d12876227
XAEA2112 <llanas.jm@hotmail.com>
2024-08-19 10:26:07 -0600

    Se añadio un diseño a la página del login, además de corregir algunos aspectos y elementos como el branding e implementar una lógica para la muestra de errores

--- frontend/src/pages/Login/LoginPage.vue
+++ frontend/src/pages/Login/LoginPage.vue
@@ -1,24 +1,49 @@
 <script setup lang="ts">
-import { IonContent } from '@ionic/vue';
-import { IonPage } from '@ionic/vue';
-import { ref } from 'vue'
+import { IonContent, IonPage } from '@ionic/vue';
+import { ref, watch } from 'vue'
 import { useRouter } from 'vue-router';
 import { useAuthStore } from "@/stores/auth"
+import azulImage from "@/assets/images/Azul.png"
+import { authErrorHandler } from '@/helpers/authErrorsHandler';
 
 const auth = useAuthStore()
 const router = useRouter()
+const isLoading = ref(false);
+
 const form = ref({
     email: '',
     password: ''
 })
 
-async function submit() {
-    try{
-    await auth.login(form)
-    router.push({ name: 'home' })
-    } catch (error) {
-        console.error('Error durante el login', error)
+
+const errorMessage = ref('');
+const emailError = ref(false);
+const passwordError = ref(false);
+
+watch(form, (newForm) => {
+    if (newForm.email && newForm.password) {
+        errorMessage.value = '';
+        emailError.value = false;
+        passwordError.value = false;
     }
+}, { deep: true });
+
+async function submit() {
+
+    try {
+    isLoading.value = true;
+    const result = await auth.login(form);
+    if (result.errors) throw result;
+    if (result.errors) return;
+    router.push("/");
+        } catch (error: any) {
+    authErrorHandler(error);
+        } finally {
+    isLoading.value = false;
+            }
+
+    emailError.value = !form.value.email;
+    passwordError.value = !form.value.password;
 
 }
 
@@ -27,20 +52,55 @@ async function submit() {
 <template>
     <IonPage>
         <IonContent>
-            <form @submit.prevent="submit">
-                <label>
-                    <span class="text-red-500">Email</span>
-                    <input type="email" v-model="form.email"/>
-                </label>
-
-                <label>
-                    <span>Password</span>
-                    <input type="password" v-model="form.password"/>
-                </label>
-
-                <button type="submit">Login</button>
-
-            </form>
+            <div class="flex flex-col w-full overflow-hidden relative min-h-screen backgroundcolor items-center justify-center g-0 px-4">
+                <div class="justify-center items-center w-full card lg:flex max-w-md">
+                    <div class="w-full card-body">
+                        
+                    
+                        <form @submit.prevent="submit" class="m-8">
+                            <div class="mb-4">
+                                <label for="forUsername" class="block text-sm font-semibold mb-2 text-gray-600">Email</label>
+                                <input type="email" placeholder="Correo Electrónico" id="forUsername" v-model="form.email" :class="{'border-red-500': emailError}"
+                                    class="py-3 px-4 block w-full border-2 border-gray-200 rounded-md text-sm focus:border-blue-600 focus:ring-0"
+                                    aria-describedby="hs-input-helper-text">
+                            </div>
+                            <div class="mb-6">
+                                <label for="forPassword" class="block text-sm font-semibold mb-2 text-gray-600">Contraseña</label>
+                                <input type="password" placeholder="Contraseña" id="forPassword" v-model="form.password" :class="{'border-red-500': passwordError}"
+                                    class="py-3 px-4 block w-full border-2 border-gray-200 rounded-md text-sm focus:border-blue-600 focus:ring-0"
+                                    aria-describedby="hs-input-helper-text">
+                            </div>
+                            
+                            <div class="grid my-6">
+                                <button type="submit" class="btn py-[10px] text-base text-white font-medium hover:bg-blue-700">Iniciar Sesión</button>
+                            </div>
+                        
+                        </form>
+                    </div>
+                </div>
+            </div>
         </IonContent>
     </IonPage>
 </template>
+
+<style scoped>
+/* Add any additional styles here if needed */
+.backgroundcolor {
+    background-color: #eff4ff;
+}
+.card {
+    background-color: white;
+    border-radius: 0.5rem;
+    box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+}
+.btn {
+    background-color: #3b82f6;
+    border-radius: 0.375rem;
+}
+.custom-margin{
+    margin-left: 124px;
+}
+template {
+  font-family: 'Plus Jakarta Sans', sans-serif;
+}
+</style>
