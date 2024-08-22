1908c7a855be524f1ca928eee43afeac21085468
Wilovy09 <srwilovy09@gmail.com>
2024-08-22 11:53:57 -0600

    :sparkles: Se separan archivos de css y se mejora el layout

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -1,7 +1,47 @@
 <script lang="ts" setup>
-import { IonRouterOutlet } from "@ionic/vue";
+import {
+  IonButtons,
+  IonHeader,
+  IonIcon,
+  IonContent,
+  IonMenu,
+  IonMenuToggle,
+  IonPage,
+  IonRouterOutlet,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/vue";
+import { chevronBackOutline, personCircleSharp } from "ionicons/icons";
 </script>
 <template>
-  <div class="w-full h-16 bg-primary"></div>
-  <IonRouterOutlet />
+  <IonMenu side="end" content-id="main-content">
+    <IonHeader mode="md">
+      <ion-toolbar>
+        <IonMenuToggle class="flex items-center">
+          <IonIcon
+            class="h-8 w-8 text-black cursor-pointer"
+            :icon="chevronBackOutline"
+          ></IonIcon
+        ></IonMenuToggle>
+      </ion-toolbar>
+    </IonHeader>
+  </IonMenu>
+  <ion-page id="main-content">
+    <IonHeader mode="md">
+      <IonToolbar color="primary">
+        <IonTitle size="large">AIMS</IonTitle>
+        <IonButtons slot="end">
+          <IonMenuToggle class="flex items-center">
+            <IonIcon
+              class="h-12 w-12 text-white cursor-pointer"
+              :icon="personCircleSharp"
+            ></IonIcon>
+          </IonMenuToggle>
+        </IonButtons>
+      </IonToolbar>
+    </IonHeader>
+    <IonContent>
+      <IonRouterOutlet />
+    </IonContent>
+  </ion-page>
 </template>
