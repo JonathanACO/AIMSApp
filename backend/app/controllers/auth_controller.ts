c52b6188af79f09da834c27e9c68b64a660bb469
XAEA2112 <llanas.jm@hotmail.com>
2024-08-22 15:37:26 -0600

    Correcciones del formateador

--- backend/app/controllers/auth_controller.ts
+++ backend/app/controllers/auth_controller.ts
@@ -3,34 +3,30 @@ import { loginValidator, registerValidator } from '#validators/auth'
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class AuthController {
-  
   async register({ request }: HttpContext) {
-
     const data = await request.validateUsing(registerValidator)
     const user = await User.create(data)
 
     return User.accessTokens.create(user)
   }
-  
+
   async login({ request }: HttpContext) {
     const { email, password } = await request.validateUsing(loginValidator)
     const user = await User.verifyCredentials(email, password)
-    
+
     return User.accessTokens.create(user)
   }
-  
+
   async logout({ auth }: HttpContext) {
     const user = auth.user!
     await User.accessTokens.delete(user, user.currentAccessToken.identifier)
-    return { message: 'success'}
-
+    return { message: 'success' }
   }
-  
-  async me({  auth }: HttpContext) {
+
+  async me({ auth }: HttpContext) {
     await auth.check()
     return {
       user: auth.user,
     }
   }
-  
 }
