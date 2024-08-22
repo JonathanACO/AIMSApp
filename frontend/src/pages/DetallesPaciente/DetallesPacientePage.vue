0c3a1e5b50fba22238c03b55e9642cd19d5db6ed
Wilovy09 <srwilovy09@gmail.com>
2024-08-22 11:56:36 -0600

    :sparkles: Se agrega faltantes para acomodarse al layout

--- frontend/src/pages/DetallesPaciente/DetallesPacientePage.vue
+++ frontend/src/pages/DetallesPaciente/DetallesPacientePage.vue
@@ -8,54 +8,64 @@ import {
 </script>
 
 <template>
-  <IonContent class="w-full">
-    <h1 class="title">Detalles de paciente</h1>
-    <div class="grid grid-cols-2 gap-4 my-8">
-      <section class="bg-secondary rounded-md min-h-64">
-        <p>Información completa del paciente, habitación, fechas, etc.</p>
-      </section>
-      <section class="h-full">
-        <article class="bg-secondary rounded-md min-h-28">
-          <p>Texto informativo para iniciar el proceso de consulta.</p>
-        </article>
-        <div class="grid gap-4 mt-4">
-          <button class="text-white bg-primary rounded-2xl p-4">
-            Iniciar consulta
-          </button>
-          <button class="text-white rounded-2xl bg-black p-4">
-            Ver última consulta
-          </button>
-        </div>
-      </section>
-    </div>
-    <div class="">
-      <h2 class="title">Historial de consultas</h2>
-      <!-- TODO: Agregar v-for y lógica de mostrarlos -->
-      <section class="grid gap-4 mt-8">
-        <div
-          class="border border-tertiary p-1 flex justify-between items-center"
-        >
-          <p>INFO DE CONSULTA</p>
-          <div class="flex gap-x-1">
-            <IonButton size="small" class="text-white" aria-label="valoracion">
-              <IonIcon slot="icon-only" :icon="starOutline"></IonIcon>
-            </IonButton>
-            <IonButton size="small" class="text-white" aria-label="sugerencias">
-              <IonIcon
-                slot="icon-only"
-                :icon="chatboxEllipsesOutline"
-              ></IonIcon>
-            </IonButton>
-            <IonButton
-              size="small"
-              class="text-white"
-              aria-label="plan-de-cuidados"
-            >
-              <IonIcon slot="icon-only" :icon="readerOutline"></IonIcon>
-            </IonButton>
+  <IonPage>
+    <IonContent class="ion-padding" :fullscreen="true">
+      <h1 class="title">Detalles de paciente</h1>
+      <div class="grid grid-cols-2 gap-4 my-8">
+        <section class="bg-secondary rounded-md min-h-64">
+          <p>Información completa del paciente, habitación, fechas, etc.</p>
+        </section>
+        <section class="h-full">
+          <article class="bg-secondary rounded-md min-h-28">
+            <p>Texto informativo para iniciar el proceso de consulta.</p>
+          </article>
+          <div class="grid gap-4 mt-4">
+            <button class="text-white bg-primary rounded-2xl p-4">
+              Iniciar consulta
+            </button>
+            <button class="text-white rounded-2xl bg-black p-4">
+              Ver última consulta
+            </button>
           </div>
-        </div>
-      </section>
-    </div>
-  </IonContent>
+        </section>
+      </div>
+      <div class="">
+        <h2 class="title">Historial de consultas</h2>
+        <!-- TODO: Agregar v-for y lógica de mostrarlos -->
+        <section class="grid gap-4 mt-8">
+          <div
+            class="border border-tertiary p-1 flex justify-between items-center"
+          >
+            <p>INFO DE CONSULTA</p>
+            <div class="flex gap-x-1">
+              <IonButton
+                size="small"
+                class="text-white"
+                aria-label="valoracion"
+              >
+                <IonIcon slot="icon-only" :icon="starOutline"></IonIcon>
+              </IonButton>
+              <IonButton
+                size="small"
+                class="text-white"
+                aria-label="sugerencias"
+              >
+                <IonIcon
+                  slot="icon-only"
+                  :icon="chatboxEllipsesOutline"
+                ></IonIcon>
+              </IonButton>
+              <IonButton
+                size="small"
+                class="text-white"
+                aria-label="plan-de-cuidados"
+              >
+                <IonIcon slot="icon-only" :icon="readerOutline"></IonIcon>
+              </IonButton>
+            </div>
+          </div>
+        </section>
+      </div>
+    </IonContent>
+  </IonPage>
 </template>
