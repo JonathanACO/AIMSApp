73bb3cb39890ddc6675284460c30a88c5ec674f4
Wilovy09 <srwilovy09@gmail.com>
2024-08-22 09:40:47 -0600

    :fire: Se elimina `IonContent`

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -1,9 +1,7 @@
 <script lang="ts" setup>
-import { IonRouterOutlet, IonContent } from "@ionic/vue";
+import { IonRouterOutlet } from "@ionic/vue";
 </script>
 <template>
   <div class="w-full h-16 bg-primary"></div>
-  <IonContent>
-    <IonRouterOutlet />
-  </IonContent>
+  <IonRouterOutlet />
 </template>
