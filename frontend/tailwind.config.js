ff08f3b81d3f75a25722b42e884ad01b96321159
Wilovy09 <srwilovy09@gmail.com>
2024-08-22 09:39:26 -0600

    :sparkles: Se agrega `index.html` y la font Inter

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{vue,html,css,ts,js,tsx,jsx}"],
+  content: ["./index.html","./src/**/*.{vue,html,css,ts,js,tsx,jsx}"],
   theme: {
     extend: {
       colors: {
         primary: "#1365a8",
         secondary: "#f3f7fe",
         tertiary: "#666667"
+      },
+      fontFamily: {
+        sans: [
+          "Inter",
+          "system-ui",
+          "-apple-system",
+          "Arial",
+          "sans-serif",
+        ],
       },
     },
   },
