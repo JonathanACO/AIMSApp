d1b7da863d835d766901a3a663ac148bca09c471
JonathanACO <jonathanandresco@gmail.com>
2024-08-26 11:27:59 -0600

    Se agregan valores hardcodeados

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -1,4 +1,6 @@
 <script lang="ts" setup>
+import { ShiftEnum, RoleEnum, SexEnum, Staff } from "@/entities/Staff";
+import { nameFormatter } from "@/helpers/nameFormatter";
 import {
   IonButtons,
   IonHeader,
@@ -16,12 +18,29 @@ import {
   personCircleOutline,
   personCircleSharp,
 } from "ionicons/icons";
+import { ref } from "vue";
+
+const nurse = ref<Staff>({
+  id: 1,
+  name: "Jonathan Andrés Cano Ornelas",
+  password: "password",
+  sex: SexEnum.masculino,
+  ageInYears: 19,
+  shift: ShiftEnum.morning,
+  workExperienceInMonths: 10,
+  role: RoleEnum.nurse,
+  createdAt: new Date(),
+  modifiedAt: new Date(),
+  roomId: 1,
+  patientId: 1,
+});
+const formattedName = nameFormatter(nurse.value.name);
 </script>
 <template>
   <IonMenu side="end" content-id="main-content">
     <IonHeader mode="md">
       <ion-toolbar color="light" class="h-16 flex items-center">
-        <IonMenuToggle class="flex items-center">
+        <IonMenuToggle class="ml-2.5 flex items-center">
           <IonIcon
             class="h-8 w-8 text-black cursor-pointer"
             :icon="chevronBackOutline"
@@ -35,25 +54,26 @@ import {
           <IonIcon class="h-20 w-20" :icon="personCircleOutline"></IonIcon>
         </div>
         <div></div>
-        <h1 class="-mt-2 text-xl">J. Cano O.</h1>
-        <h2 class="text-xl mt-10 mb-3">Información</h2>
+        <h1 class="-mt-2 text-xl mb-10">{{ formattedName }}</h1>
+        <hr />
+        <h2 class="text-xl font-semibold mt-8 mb-4">Información</h2>
         <div class="flex w-full justify-center">
           <div class="text-left w-max">
-            <div class="w-max my-1">
+            <div class="w-max my-1.5">
               <div class="w-36 inline-block font-semibold">ID:</div>
-              <p class="inline-block">PF001</p>
+              <p class="inline-block">{{ `PF00${nurse.id}` }}</p>
             </div>
-            <div class="w-max my-1">
+            <div class="w-max my-1.5">
               <div class="w-36 inline-block font-semibold">Turno:</div>
-              <p class="inline-block">Matutino</p>
+              <p class="inline-block">{{ nurse.shift }}</p>
             </div>
-            <div class="w-max my-1">
+            <div class="w-max my-1.5">
               <div class="w-36 inline-block font-semibold">Habitación:</div>
-              <p class="inline-block">Cardiología</p>
+              <p class="inline-block">{{ nurse.roomId }}</p>
             </div>
-            <div class="w-max my-1">
+            <div class="w-max my-1.5">
               <div class="w-36 inline-block font-semibold">Paciente:</div>
-              <p class="inline-block">P001</p>
+              <p class="inline-block">{{ `P00${nurse.patientId}` }}</p>
             </div>
           </div>
         </div>
@@ -70,6 +90,7 @@ import {
       <IonToolbar class="h-16 flex items-center" color="primary">
         <IonTitle class="font-semibold text-2xl">AIMS</IonTitle>
         <IonButtons slot="end">
+          <h3 class="mr-2">{{ formattedName }}</h3>
           <IonMenuToggle class="flex items-center">
             <IonIcon
               class="h-12 w-12 pr-2 text-white cursor-pointer"
