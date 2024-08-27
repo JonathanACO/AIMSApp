7719315ffccc1f44fadb50efcc61e2e8cd0f1334
JonathanACO <jonathanandresco@gmail.com>
2024-08-27 10:01:04 -0600

    Se agrega fuente al layout

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -48,38 +48,42 @@ const formattedName = nameFormatter(nurse.value.name);
         ></IonMenuToggle>
       </ion-toolbar>
     </IonHeader>
-    <IonContent color="light" class="relative">
+    <IonContent color="light" class="relative font-sans">
       <div class="w-full flex flex-col text-center mt-10">
         <div>
           <IonIcon class="h-20 w-20" :icon="personCircleOutline"></IonIcon>
         </div>
         <div></div>
-        <h1 class="-mt-2 text-xl mb-10">{{ formattedName }}</h1>
+        <h1 class="-mt-2 text-xl mb-10 font-semibold">{{ formattedName }}</h1>
         <hr />
-        <h2 class="text-xl font-semibold mt-8 mb-4">Información</h2>
+        <h2 class="text-xl font-bold mt-8 mb-4">Información</h2>
         <div class="flex w-full justify-center">
           <div class="text-left w-max">
             <div class="w-max my-1.5">
-              <div class="w-36 inline-block font-semibold">ID:</div>
-              <p class="inline-block">{{ `PF00${nurse.id}` }}</p>
+              <div class="w-36 inline-block font-bold">ID:</div>
+              <p class="inline-block font-medium">{{ `PF00${nurse.id}` }}</p>
             </div>
             <div class="w-max my-1.5">
-              <div class="w-36 inline-block font-semibold">Turno:</div>
-              <p class="inline-block">{{ nurse.shift }}</p>
+              <div class="w-36 inline-block font-bold">Turno:</div>
+              <p class="inline-block font-medium">{{ nurse.shift }}</p>
             </div>
             <div class="w-max my-1.5">
-              <div class="w-36 inline-block font-semibold">Habitación:</div>
-              <p class="inline-block">{{ nurse.roomId }}</p>
+              <div class="w-36 inline-block font-bold">Habitación:</div>
+              <p class="inline-block font-medium">{{ nurse.roomId }}</p>
             </div>
             <div class="w-max my-1.5">
-              <div class="w-36 inline-block font-semibold">Paciente:</div>
-              <p class="inline-block">{{ `P00${nurse.patientId}` }}</p>
+              <div class="w-36 inline-block font-bold">Paciente:</div>
+              <p class="inline-block font-medium">
+                {{ `P00${nurse.patientId}` }}
+              </p>
             </div>
           </div>
         </div>
       </div>
       <IonButtons class="flex justify-center absolute bottom-0 inset-x-0 mb-10">
-        <button class="text-white rounded-2xl bg-black py-3 px-16">
+        <button
+          class="text-white rounded-2xl bg-black py-3 px-16 font-semibold"
+        >
           Cerrar sesión
         </button>
       </IonButtons>
@@ -90,7 +94,7 @@ const formattedName = nameFormatter(nurse.value.name);
       <IonToolbar class="h-16 flex items-center pr-5" color="primary">
         <IonTitle class="font-semibold text-2xl">AIMS</IonTitle>
         <IonButtons slot="end">
-          <h3 class="mr-2">{{ formattedName }}</h3>
+          <h3 class="mr-2 -mb-0.5 font-medium">{{ formattedName }}</h3>
           <IonMenuToggle class="flex items-center">
             <IonIcon
               class="h-12 w-12 text-white cursor-pointer"
