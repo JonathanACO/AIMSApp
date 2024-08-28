2138bcf32a11eea96635e6e9b4c2ce0ece55d566
XAEA2112 <llanas.jm@hotmail.com>
2024-08-28 10:29:02 -0600

    De definió una sola entidad "Staff"

--- frontend/src/entities/Staff.ts
+++ frontend/src/entities/Staff.ts
@@ -1,11 +1,17 @@
+type Sex = "Male" | "Female";
+type Shift = "Morning" | "Evening" | "Night";
+type Role = "Admin" | "Nurse";
+
 export type Staff = {
-    id: number;
-    nombre: string;
-    sexo: "Masculino" | "Femenino";
-    edad: number;
-    turno: "Matutino" | "Vespertino" | "Nocturno";
-    experiencia_laboral: string;
-    habitacionId: number;
-    created_at: Date;
-    modified_at: Date | null;
-  };
+  id: number;
+  name: string | null;
+  email: string;
+  sex: Sex;
+  age: number;
+  shift: Shift;
+  workExperienceInMonths: number | null;
+  roomId: number | null;
+  role: Role;
+  createdAt: Date;
+  modifiedAt: Date;
+};
--- frontend/src/entities/user.ts
+++
@@ -1,7 +0,0 @@
-export type User = {
-    id: number
-    fullname: string | null
-    email: string
-    createdAt: Date
-    updatedAt: Date
-}
