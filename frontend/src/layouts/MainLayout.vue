d34d1a5f1b0c6379703a0c4919a66e0f58ee6deb
XAEA2112 <llanas.jm@hotmail.com>
2024-08-28 10:30:53 -0600

    Se eliminó el nav

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -1,19 +1,11 @@
 <script lang="ts" setup>
-import { defineComponent, DefineComponent } from "vue";
 import { IonRouterOutlet, IonContent, IonPage } from "@ionic/vue";
-
-
 </script>
 <template>
   <IonPage>
-  <div class="w-full h-16 bg-primary"></div>
-  <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/register">Register</RouterLink>
-      <RouterLink to="/login">Login</RouterLink>
-  </nav>
-  <IonContent>
-    <IonRouterOutlet />
-  </IonContent>
-</IonPage>
+    <div class="w-full h-16 bg-primary"></div>
+    <IonContent>
+      <IonRouterOutlet />
+    </IonContent>
+  </IonPage>
 </template>
