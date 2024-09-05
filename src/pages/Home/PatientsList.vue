edf31e7b184143c12e4ef872b780b1a0e35625f6
JonathanACO <jonathanandresco@gmail.com>
2024-09-05 11:43:35 -0600

    Se cambia nombre de la página

--- src/pages/Home/PatientsList.vue
+++ src/pages/Home/PatientsList.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
 import { IonContent, IonPage } from "@ionic/vue";
 import PatientTable from "./components/PatientTable.vue";
+
+/* 
+  1-Obtener los pacientes del enfermero
+  2-Obtener la habitación del paciente/Obtener ultima consulta completada y meterlos en un objeto
+*/
 </script>
 <template>
   <IonPage>
