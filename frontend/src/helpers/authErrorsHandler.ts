29ead35e6828defd5fcc44af14b70af282df96de
XAEA2112 <llanas.jm@hotmail.com>
2024-09-10 16:37:03 -0600

    Se separó el mapeo de errores del handler de errores y se hizo de este un handler genérico.

--- frontend/src/helpers/authErrorsHandler.ts
+++ frontend/src/helpers/authErrorsHandler.ts
@@ -1,25 +1,32 @@
+import { AxiosError } from "axios";
 import { showErrorToast } from "./swalFunctions";
+import { errorMessagesMap } from "./errorMessagesMap";
 
-const errorMessagesMap: Record<string, string> = {
-  "Invalid user credentials": "Nombre o contraseña incorrectos",
-  "The password field must be defined": "Introduzca una contraseña",
-  "The name field must be defined": "Introduzca su nombre",
-  "The password field must have at least 8 characters":
-    "La contraseña debe tener al menos 8 carateres",
-  "Internal Server Error": "Error interno del servidor",
-  "Failed to fetch":
-    "No se pudo conectar con el servidor. Por favor, verifica tu conexión a Internet o intenta de nuevo más tarde",
-};
+export function errorHandler(error: unknown) {
+  if (error instanceof AxiosError) {
+    let errorMessage = "Ocurrió un error";
+    if (error.response && error.response.data) {
+      const responseData = error.response.data;
 
-export function authErrorHandler(error: any) {
-  if (error.errors && Array.isArray(error.errors)) {
-    error.errors.forEach((err: any) => {
-      const translatedMessage = errorMessagesMap[err.message] || err.message;
-      showErrorToast(translatedMessage);
-    });
-  } else {
-    const translatedMessage =
-      errorMessagesMap[error.message] || error.message || "Ocurrió un error";
-    showErrorToast(translatedMessage);
+      if (Array.isArray(responseData.errors)) {
+        responseData.errors.forEach((err: any) => {
+          const translatedMessage =
+            errorMessagesMap[err.message] || err.message;
+          showErrorToast(translatedMessage);
+        });
+      } else {
+        const serverErrorMessage =
+          responseData.message ||
+          responseData.error ||
+          "Ocurrió un error en el servidor";
+        errorMessage =
+          errorMessagesMap[serverErrorMessage] || serverErrorMessage;
+        showErrorToast(errorMessage);
+      }
+    } else {
+      errorMessage = errorMessagesMap[error.message] || error.message;
+      showErrorToast(errorMessage);
+      showErrorToast("Ocurrio un error inesperado");
+    }
   }
 }
