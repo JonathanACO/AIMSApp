89f0dea2d55566fbb3e27c38d0c61093afed1ff8
JonathanACO <jonathanandresco@gmail.com>
2024-09-19 09:32:31 -0600

    Se hardcodea información de enfermero

--- src/stores/useAuthStore.ts
+++ src/stores/useAuthStore.ts
@@ -1,10 +1,20 @@
 import { ref } from "vue";
 import { defineStore } from "pinia";
 import { AuthRepository } from "@/repositories/authRepository";
-import { Nurse } from "@/entities/Nurse";
+import { Nurse, SexEnum, ShiftEnum } from "@/entities/Nurse";
 
 export const useAuthStore = defineStore("auth", () => {
-  const nurse = ref<Nurse | null>(null);
+  const nurse = ref<Nurse | null>({
+    id: 1,
+    name: "Jonathan Andrés Cano Ornelas",
+    password: "password",
+    sex: SexEnum.masculino,
+    ageInYears: 19,
+    workshift: ShiftEnum.morning,
+    workExperienceInMonths: 10,
+    createdAt: new Date(),
+    modifiedAt: new Date(),
+  });
   const token = ref(localStorage.getItem("token"));
 
   function authenticate(tokenValue: string | null) {
