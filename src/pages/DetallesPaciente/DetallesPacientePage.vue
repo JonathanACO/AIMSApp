c389c6e9cbf81288dc1389ae1a8026d61815e853
JonathanACO <jonathanandresco@gmail.com>
2024-09-19 09:29:18 -0600

    Se agrega IonPage y IonContent para un renderizado correcto

--- src/pages/DetallesPaciente/DetallesPacientePage.vue
+++ src/pages/DetallesPaciente/DetallesPacientePage.vue
@@ -1,48 +1,54 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { IonPage, IonContent } from "@ionic/vue";
+</script>
 
 <template>
-  <main class="w-full overflow-y-auto">
-    <h1 class="title">Detalles de paciente</h1>
-    <div class="grid grid-cols-1 md:grid-cols-2 gap-4 my-8">
-      <section class="bg-secondary rounded-md min-h-64">
-        <p>Información completa del paciente, habitación, fechas, etc.</p>
-      </section>
-      <section class="h-full">
-        <article class="bg-secondary rounded-md min-h-28">
-          <p>Texto informativo para iniciar el proceso de consulta.</p>
-        </article>
-        <div class="grid gap-4 mt-4">
-          <button class="text-white bg-primary rounded-2xl p-4">
-            Iniciar consulta
-          </button>
-          <button class="text-white rounded-2xl bg-black p-4">
-            Ver última consulta
-          </button>
+  <IonPage
+    ><IonContent class="ion-padding">
+      <main class="w-full overflow-y-auto">
+        <h1 class="title">Detalles de paciente</h1>
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-4 my-8">
+          <section class="bg-secondaryColor rounded-md min-h-64">
+            <p>Información completa del paciente, habitación, fechas, etc.</p>
+          </section>
+          <section class="h-full">
+            <article class="bg-secondaryColor rounded-md min-h-28">
+              <p>Texto informativo para iniciar el proceso de consulta.</p>
+            </article>
+            <div class="grid gap-4 mt-4">
+              <button class="text-white bg-primary rounded-2xl p-4">
+                Iniciar consulta
+              </button>
+              <button class="text-white rounded-2xl bg-black p-4">
+                Ver última consulta
+              </button>
+            </div>
+          </section>
         </div>
-      </section>
-    </div>
-    <div class="">
-      <h2 class="title">Historial de consultas</h2>
+        <div class="">
+          <h2 class="title">Historial de consultas</h2>
 
-      <!-- TODO: Agregar v-for y lógica de mostrarlos -->
-      <section class="grid gap-4 mt-8">
-        <div class="border border-tertiary p-3 flex justify-between">
-          <p>INFO DE CONSULTA</p>
-          <div class="flex">
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-          </div>
+          <!-- TODO: Agregar v-for y lógica de mostrarlos -->
+          <section class="grid gap-4 mt-8">
+            <div class="border border-tertiary p-3 flex justify-between">
+              <p>INFO DE CONSULTA</p>
+              <div class="flex">
+                <button>1</button>
+                <button>2</button>
+                <button>3</button>
+              </div>
+            </div>
+            <div class="border border-tertiary p-3 flex justify-between">
+              <p>INFO DE CONSULTA</p>
+              <div class="flex">
+                <button>1</button>
+                <button>2</button>
+                <button>3</button>
+              </div>
+            </div>
+          </section>
         </div>
-        <div class="border border-tertiary p-3 flex justify-between">
-          <p>INFO DE CONSULTA</p>
-          <div class="flex">
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-          </div>
-        </div>
-      </section>
-    </div>
-  </main>
+      </main></IonContent
+    ></IonPage
+  >
 </template>
