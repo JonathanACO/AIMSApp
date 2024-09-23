22ae838008fc43988e02ce017946227590ac3111
Wilovy09 <srwilovy09@gmail.com>
2024-09-23 08:45:55 -0600

    :fire:

--- src/utilities/getDaysDifference.ts
+++
@@ -1,8 +0,0 @@
-export function getDaysDifference(timestamp: Date): string {
-  const targetDate = new Date(timestamp);
-  const currentDate = new Date();
-  const targetDayOfYear = targetDate.getDate();
-  const currentDayOfYear = currentDate.getDate();
-  const dayDifference = currentDayOfYear - targetDayOfYear;
-  return `${dayDifference} dias`;
-}
