e0b78827b77c9402aa6c2986604eb68b9e9cb7b2
XAEA2112 <llanas.jm@hotmail.com>
2024-09-26 11:24:35 -0600

    api pendiente

--- services/api.ts
+++ services/api.ts
@@ -2,7 +2,6 @@ import axios from "axios";
 
 export async function api(method: string, url: string, payload = {}) {
   const token = localStorage.getItem("token");
-  const consultas = 
 
   const response = await axios(`http://localhost:3333/api${url}`, {
     method,
