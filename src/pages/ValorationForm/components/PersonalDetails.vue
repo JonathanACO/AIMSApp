facc21b5b972e12510cc904d3650f469a6466de7
JonathanACO <jonathanandresco@gmail.com>
2024-10-09 10:04:14 -0600

    Se realiza componente de la sección datos personales

---
+++ src/pages/ValorationForm/components/PersonalDetails.vue
@@ -0,0 +1,162 @@
+<script setup lang="ts">
+import VInputRadio from "@/components/VInputRadio.vue";
+import VInputText from "@/components/VInputText.vue";
+import VSelect from "@/components/VSelect.vue";
+import { computed, ref, watch } from "vue";
+//TODO: convertir los elementos vacios en nulos
+const datosPersonales = ref<{
+  edad: number | null;
+  sexo: "Masculino" | "Femenino" | null;
+  tallaEnCm: number | null;
+  pesoEnKg: number | null;
+  diagnosticoMedico: string | null;
+  datoSocial: "Vive solo" | "Con familia" | null;
+  datoSocialEspecificado: string | null;
+  escolaridad: string | null;
+  religion: string | null;
+  estadoCivil:
+    | "Soltero"
+    | "Casado"
+    | "Viudo"
+    | "Separado"
+    | "Unión libre"
+    | "Sin datos"
+    | null;
+}>({
+  edad: null,
+  sexo: null,
+  tallaEnCm: null,
+  pesoEnKg: null,
+  diagnosticoMedico: null,
+  datoSocial: null,
+  datoSocialEspecificado: null,
+  escolaridad: null,
+  religion: null,
+  estadoCivil: null,
+});
+
+const datosSocialesEspecificos = computed({
+  get() {
+    return datosPersonales.value.datoSocial === "Con familia"
+      ? datosSocialesEspecificosInternal.value
+      : null;
+  },
+  set(value) {
+    datosSocialesEspecificosInternal.value = !value ? null : value;
+  },
+});
+
+const datosSocialesEspecificosInternal = ref<string | null>(null);
+
+const civilStatementOptions = [
+  "Soltero",
+  "Casado",
+  "Viudo",
+  "Separado",
+  "Unión libre",
+  "Sin datos",
+];
+const sexOptions = ["Masculino", "Femenino"];
+const escolaridadOpciones = [
+  "Primaria",
+  "Secundaria",
+  "Bachillerato",
+  "Licenciatura",
+  "Postgrado",
+];
+
+watch(datosSocialesEspecificos, (value) => {
+  datosPersonales.value.datoSocialEspecificado = value;
+});
+</script>
+<template>
+  <div class="mx-4 my-6">
+    <div class="grid gap-x-6 gap-y-3 h-fit grid-cols-4 grid-rows-2 mb-3">
+      <VInputText
+        class=""
+        v-model="datosPersonales.edad"
+        type="number"
+        label="Edad"
+        label-position="top"
+      />
+      <VSelect
+        class=""
+        :options="sexOptions"
+        v-model="datosPersonales.sexo"
+        label="Sexo"
+        label-position="top"
+      />
+      <VInputText
+        class=""
+        v-model="datosPersonales.tallaEnCm"
+        type="number"
+        label="Talla"
+        label-position="top"
+        unit="cm"
+      />
+      <VInputText
+        class=""
+        v-model="datosPersonales.pesoEnKg"
+        type="number"
+        label="Peso"
+        label-position="top"
+        unit="kg"
+      />
+      <VInputText
+        class="col-span-4"
+        v-model="datosPersonales.diagnosticoMedico"
+        type="text"
+        label="Diagnóstico médico"
+        label-position="top"
+      />
+    </div>
+    <p class="col-span-4 text-stone-500 h-max mb-1.5">Datos sociales</p>
+    <div class="grid gap-x-6 min-h-8 h-fit grid-cols-5 grid-rows-1 mb-3">
+      <VInputRadio
+        v-model="datosPersonales.datoSocial"
+        value="Vive solo"
+        label="Vive solo"
+        identifier="DatosSociales"
+        class="w-max"
+      />
+      <VInputRadio
+        v-model="datosPersonales.datoSocial"
+        value="Con familia"
+        label="Con familia"
+        identifier="DatosSociales"
+        class="w-max"
+      />
+      <VInputText
+        :disabled="datosPersonales.datoSocial !== 'Con familia'"
+        class="w-full col-span-3"
+        v-model="datosSocialesEspecificos"
+        type="text"
+        label="Especificar"
+        label-position="side"
+      />
+    </div>
+    <div class="grid gap-x-6 min-h-8 h-fit grid-cols-3 grid-rows-1">
+      <VSelect
+        class=""
+        v-model="datosPersonales.escolaridad"
+        :options="escolaridadOpciones"
+        type="text"
+        label="Escolaridad"
+        label-position="top"
+      />
+      <VInputText
+        class=""
+        v-model="datosPersonales.religion"
+        type="text"
+        label="Religión"
+        label-position="top"
+      />
+      <VSelect
+        v-model="datosPersonales.estadoCivil"
+        :options="civilStatementOptions"
+        label="Estado civil"
+        label-position="top"
+      />
+    </div>
+  </div>
+</template>
