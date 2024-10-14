a10f784ca0cfc8c75322ee0e1192605a1fc9c8e8
XAEA2112 <llanas.jm@hotmail.com>
2024-10-14 11:26:02 -0600

    Se corrigió el uso del layout

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -6,16 +6,19 @@ const routes = [
     component: () => import("../pages/Login/LoginPage.vue"),
   },
   {
-    path: "/historial",
-    name: "historial",
-    meta: { requireAuth: true },
-    component: () => import("../pages/HistorialConsultas/HistorialPage.vue"),
-  },
-  {
     path: "/",
     name: "main-layout",
     meta: { requireAuth: true },
     component: () => import("../layouts/MainLayout.vue"),
+    children: [
+      {
+        path: "historial",
+        name: "historial",
+        meta: { requireAuth: true },
+        component: () =>
+          import("../pages/AppoinmentsHistory/AppointmentsHistoryPage.vue"),
+      },
+    ],
   },
 ];
 export { routes };
