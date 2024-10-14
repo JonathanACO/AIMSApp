33ab70a899b7306b38101661970e18b472a4bc3e
XAEA2112 <llanas.jm@hotmail.com>
2024-10-14 11:38:02 -0600

    Se eliminó el IonContent innecesario

--- src/pages/AppoinmentsHistory/AppointmentsHistoryPage.vue
+++ src/pages/AppoinmentsHistory/AppointmentsHistoryPage.vue
@@ -5,13 +5,7 @@ import { nameFormatter } from "@/helpers/nameFormatter";
 import { errorHandler } from "@/helpers/authErrorsHandler";
 import { AppointmentsRepository } from "@/repositories/AppointmentRepository";
 import { Appointment } from "@/entities/Appointment";
-import {
-  IonButtons,
-  IonIcon,
-  IonContent,
-  IonRouterOutlet,
-  IonSpinner,
-} from "@ionic/vue";
+import { IonButtons, IonIcon, IonRouterOutlet, IonSpinner } from "@ionic/vue";
 import {
   clipboardOutline,
   chatboxEllipsesOutline,
@@ -66,101 +60,99 @@ const formatDate = (date: string | Date) => {
 };
 </script>
 <template>
-  <IonContent>
-    <div class="px-10 py-10 font-sans">
-      <div>
-        <h1 class="font-sans text-2xl">Revisión de consulta anterior</h1>
-      </div>
-      <div
-        v-show="isLoading"
-        class="flex justify-center items-center h-full p-28"
-      >
-        <IonSpinner class="p-10" color="primary" />
-      </div>
-
-      <Swiper
-        v-if="!isLoading"
-        :modules="[Navigation, Pagination]"
-        :a11y="true"
-        :pagination="{
-          type: 'bullets',
-          el: '.swiper-pagination',
-        }"
-        :navigation="{
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        }"
-        @slide-change="handleSlideChange"
-        class="relative"
-      >
-        <SwiperSlide
-          class="p-10 items-center text-center"
-          v-for="appointment in appointments"
-          :key="appointment.id"
-        >
-          <h3 class="text-xl py-4 bg-blue-50">
-            Fecha de la consulta: {{ formatDate(appointment.createdAt)
-            }}<br />Enfermero(a):
-            {{ formattedName }}
-          </h3>
-          <div class="py-8 px-10 flex bg-white">
-            <IonButtons class="flex flex-col items-center w-1/3 h-40">
-              <button
-                class="w-24 h-24 rounded-full bg-sky-300 hover:bg-blue-400 flex items-center justify-center cursor-pointer"
-              >
-                <IonIcon
-                  :icon="clipboardOutline"
-                  class="text-black size-14"
-                ></IonIcon>
-              </button>
-              <IonLabel class="mt-2 text-sm text-center">Valoración</IonLabel>
-            </IonButtons>
-            <IonButtons class="flex flex-col items-center w-1/3 h-40">
-              <button
-                class="w-24 h-24 rounded-full bg-sky-300 hover:bg-blue-400 flex items-center justify-center cursor-pointer"
-              >
-                <IonIcon
-                  :icon="chatboxEllipsesOutline"
-                  class="text-black size-14"
-                ></IonIcon>
-              </button>
-              <IonLabel class="mt-2 text-sm text-center">Sugerencias</IonLabel>
-            </IonButtons>
-            <IonButtons class="flex flex-col items-center w-1/3 h-40">
-              <button
-                class="w-24 h-24 rounded-full bg-sky-300 hover:bg-blue-400 flex items-center justify-center cursor-pointer"
-              >
-                <IonIcon
-                  :icon="heartOutline"
-                  class="text-black size-14"
-                ></IonIcon>
-              </button>
-              <div class="h-14 flex items-start">
-                <IonLabel class="mt-2 text-sm text-center"
-                  >Plan de cuidados individualizados</IonLabel
-                >
-              </div>
-            </IonButtons>
-          </div>
-          <div class="flex justify-between mt-4 px-2 text-lg">
-            <span v-if="currentIndex > 0">
-              {{ formatDate(appointments[currentIndex - 1].createdAt) }}
-            </span>
-
-            <span v-else class="invisible">Placeholder</span>
-            <span v-if="currentIndex < appointments.length - 1">
-              {{ formatDate(appointments[currentIndex + 1].createdAt) }}
-            </span>
-
-            <span v-else class="invisible">Placeholder</span>
-          </div>
-        </SwiperSlide>
-        <div class="swiper-pagination !bottom-0 !top-auto"></div>
-
-        <div class="swiper-button-prev !text-blue-800 !mt-36 !left-0"></div>
-        <div class="swiper-button-next !text-blue-800 !mt-36 !right-0"></div>
-      </Swiper>
+  <div class="px-10 py-10 font-sans">
+    <div>
+      <h1 class="font-sans text-2xl">Revisión de consulta anterior</h1>
     </div>
-    <IonRouterOutlet />
-  </IonContent>
+    <div
+      v-show="isLoading"
+      class="flex justify-center items-center h-full p-28"
+    >
+      <IonSpinner class="p-10" color="primary" />
+    </div>
+
+    <Swiper
+      v-if="!isLoading"
+      :modules="[Navigation, Pagination]"
+      :a11y="true"
+      :pagination="{
+        type: 'bullets',
+        el: '.swiper-pagination',
+      }"
+      :navigation="{
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      }"
+      @slide-change="handleSlideChange"
+      class="relative"
+    >
+      <SwiperSlide
+        class="p-10 items-center text-center"
+        v-for="appointment in appointments"
+        :key="appointment.id"
+      >
+        <h3 class="text-xl py-4 bg-blue-50">
+          Fecha de la consulta: {{ formatDate(appointment.createdAt)
+          }}<br />Enfermero(a):
+          {{ formattedName }}
+        </h3>
+        <div class="py-8 px-10 flex bg-white">
+          <IonButtons class="flex flex-col items-center w-1/3 h-40">
+            <button
+              class="w-24 h-24 rounded-full bg-sky-300 hover:bg-blue-400 flex items-center justify-center cursor-pointer"
+            >
+              <IonIcon
+                :icon="clipboardOutline"
+                class="text-black size-14"
+              ></IonIcon>
+            </button>
+            <IonLabel class="mt-2 text-sm text-center">Valoración</IonLabel>
+          </IonButtons>
+          <IonButtons class="flex flex-col items-center w-1/3 h-40">
+            <button
+              class="w-24 h-24 rounded-full bg-sky-300 hover:bg-blue-400 flex items-center justify-center cursor-pointer"
+            >
+              <IonIcon
+                :icon="chatboxEllipsesOutline"
+                class="text-black size-14"
+              ></IonIcon>
+            </button>
+            <IonLabel class="mt-2 text-sm text-center">Sugerencias</IonLabel>
+          </IonButtons>
+          <IonButtons class="flex flex-col items-center w-1/3 h-40">
+            <button
+              class="w-24 h-24 rounded-full bg-sky-300 hover:bg-blue-400 flex items-center justify-center cursor-pointer"
+            >
+              <IonIcon
+                :icon="heartOutline"
+                class="text-black size-14"
+              ></IonIcon>
+            </button>
+            <div class="h-14 flex items-start">
+              <IonLabel class="mt-2 text-sm text-center"
+                >Plan de cuidados individualizados</IonLabel
+              >
+            </div>
+          </IonButtons>
+        </div>
+        <div class="flex justify-between mt-4 px-2 text-lg">
+          <span v-if="currentIndex > 0">
+            {{ formatDate(appointments[currentIndex - 1].createdAt) }}
+          </span>
+
+          <span v-else class="invisible">Placeholder</span>
+          <span v-if="currentIndex < appointments.length - 1">
+            {{ formatDate(appointments[currentIndex + 1].createdAt) }}
+          </span>
+
+          <span v-else class="invisible">Placeholder</span>
+        </div>
+      </SwiperSlide>
+      <div class="swiper-pagination !bottom-0 !top-auto"></div>
+
+      <div class="swiper-button-prev !text-blue-800 !mt-36 !left-0"></div>
+      <div class="swiper-button-next !text-blue-800 !mt-36 !right-0"></div>
+    </Swiper>
+  </div>
+  <IonRouterOutlet />
 </template>
