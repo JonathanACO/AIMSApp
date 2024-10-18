9dc16ccdf31dc3b76ad25d1e52a7bc6e0ccd19e3
Leon Casas <leonn.casass@gmail.com>
2024-10-17 23:48:58 -0600

    Se elimina PatientRepository y se modifica PatientsRepository para agregar los metodos correctos

--- src/repositories/PatientRepository.ts
+++
@@ -1,8 +0,0 @@
-import { Patient } from "@/entities/Patient";
-import { api } from "@/services/api";
-
-export class PatientRespository {
-  public async getDetails(id: number): Promise<Patient> {
-    return await api("GET", `/patient-details/${id}`);
-  }
-}
--- src/repositories/PatientsRepository.ts
+++ src/repositories/PatientsRepository.ts
@@ -1,6 +1,11 @@
+import { Patient } from "@/entities/Patient";
 import { api } from "@/services/api";
 
 export class PatientsRepository {
+  public static async fetchById(id: number): Promise<Patient> {
+    return await api("GET", `/patients/${id}`);
+  }
+
   public static async fetchByNurseId(id: number): Promise<
     {
       patient: number;
