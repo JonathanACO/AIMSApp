d6d60d96e27f9ed22d103c45c87911b8e9e0bae9
Leon Casas <leonn.casass@gmail.com>
2024-10-18 01:12:56 -0600

    - Se agrega conditional rendering para mostrar el contenido dependiendo al estado de carga. - Se agrega router.push al boton de ultima consulta.

--- src/pages/PatientDetail/PatientDetailPage.vue
+++ src/pages/PatientDetail/PatientDetailPage.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { computed, onMounted, ref } from "vue";
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 import { IonContent, IonSpinner, IonPage } from "@ionic/vue";
 import { RoomsRepository } from "@/repositories/RoomsRepository";
 import { formatPatientId } from "../../helpers/formatPatientId";
@@ -15,6 +15,7 @@ import { AppointmentsRepository } from "../../repositories/AppointmentsRepositor
 import PatientAppointmentsTable from "./components/PatientAppointmentsTable.vue";
 
 const route = useRoute();
+const router = useRouter();
 const isLoading = ref(false);
 
 const patientId = Number(route.params.id);
@@ -79,69 +80,77 @@ onMounted(async () => {
   <IonPage>
     <IonContent class="ion-padding" :fullscreen="true">
       <div
-        v-show="isLoading"
+        v-if="isLoading"
         class="flex justify-center items-center h-full p-28"
       >
         <IonSpinner class="p-10" color="primary" />
       </div>
-      <h1 class="title">Detalles de paciente</h1>
-      <div class="grid grid-cols-2 gap-4 my-8">
-        <section
-          v-if="patient"
-          class="bg-secondary grid rounded-md min-h-64 p-3 text-lg"
-        >
-          <p class="text-start mb-2 font-semibold text-2xl">
-            {{
-              patientIdentifier ??
-              "Error al obtener identificador del paciente."
-            }}
-          </p>
-          <span>
-            <p class="font-semibold">Fecha de entrada</p>
-            <p>
+      <div v-else>
+        <h1 class="title">Detalles de paciente</h1>
+        <div v-if="patient" class="grid grid-cols-2 gap-4 my-8">
+          <section
+            v-if="patient"
+            class="bg-secondary grid rounded-md min-h-64 p-3 text-lg"
+          >
+            <p class="text-start mb-2 font-semibold text-2xl">
               {{
-                formatDateToLong(patient.createdAt) ??
-                "Error al obtener fecha de entrada."
+                patientIdentifier ??
+                "Error al obtener identificador del paciente."
               }}
             </p>
-          </span>
-          <span v-if="room">
-            <p class="font-semibold">Habitación</p>
-            <p>
-              {{ room.name ?? "Error al obtener nombre de habitación." }}
-            </p>
-          </span>
-          <span v-if="appointments">
-            <p class="font-semibold">Número de consultas</p>
-            <p>
-              {{ appointments.length ?? "Error al obtener consultas" }}
-            </p>
-          </span>
-          <span>
-            <p class="font-semibold">Días en el hospital</p>
-            <p>
-              {{ daysInHospital ?? "Error al obtener días en hospital." }}
-            </p>
-          </span>
-        </section>
-        <section class="h-full">
-          <div class="grid gap-4">
-            <button
-              class="text-white bg-primary rounded-lg p-4 hover:bg-tertiary transition-all"
-            >
-              Iniciar consulta
-            </button>
-            <button
-              class="text-white rounded-lg bg-black p-4 hover:bg-tertiary transition-all"
-            >
-              Ver última consulta
-            </button>
-          </div>
-        </section>
-      </div>
-      <div class="overflow-y-auto h-3/5">
-        <h2 class="title mb-5">Historial de consultas</h2>
-        <PatientAppointmentsTable :appointments="appointments" />
+            <span>
+              <p class="font-semibold">Fecha de entrada</p>
+              <p>
+                {{
+                  formatDateToLong(patient.createdAt) ??
+                  "Error al obtener fecha de entrada."
+                }}
+              </p>
+            </span>
+            <span v-if="room">
+              <p class="font-semibold">Habitación</p>
+              <p>
+                {{ room.name ?? "Error al obtener nombre de habitación." }}
+              </p>
+            </span>
+            <span v-if="appointments">
+              <p class="font-semibold">Número de consultas</p>
+              <p>
+                {{ appointments.length ?? "Error al obtener consultas" }}
+              </p>
+            </span>
+            <span>
+              <p class="font-semibold">Días en el hospital</p>
+              <p>
+                {{ daysInHospital ?? "Error al obtener días en hospital." }}
+              </p>
+            </span>
+          </section>
+          <section class="h-full">
+            <div class="grid gap-4">
+              <button
+                class="text-white bg-primary rounded-lg p-4 hover:bg-tertiary transition-all"
+              >
+                Iniciar consulta
+              </button>
+              <button
+                @click="
+                  router.push({
+                    name: 'appointments-history',
+                    params: { id: patient.id },
+                  })
+                "
+                class="text-white rounded-lg bg-black p-4 hover:bg-tertiary transition-all"
+              >
+                Ver última consulta
+              </button>
+            </div>
+          </section>
+        </div>
+        <div class="overflow-y-auto h-3/5">
+          <h2 class="title mb-5">Historial de consultas</h2>
+          <PatientAppointmentsTable :appointments="appointments" />
+        </div>
       </div>
     </IonContent>
   </IonPage>
