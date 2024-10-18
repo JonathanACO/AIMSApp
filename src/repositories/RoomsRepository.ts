656c931388755f749f10f8f16b2567c7d2589e8e
Leon Casas <leonn.casass@gmail.com>
2024-10-17 23:53:14 -0600

    Se refactoriza RoomsRepository

--- src/repositories/RoomsRepository.ts
+++ src/repositories/RoomsRepository.ts
@@ -1,8 +1,8 @@
 import { Room } from "@/entities/Room";
 import { api } from "@/services/api";
 
-export class RoomsRespository {
-  public async find(id: number): Promise<Room> {
+export class RoomsRepository {
+  public static async fetchById(id: number): Promise<Room> {
     return await api("GET", `/rooms/${id}`);
   }
 }
