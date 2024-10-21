35915a1762738a05773a5b0a611d1904de841390
Leon Casas <leonn.casass@gmail.com>
2024-10-20 19:58:36 -0600

    Se crea evento para emitir las opciones seleccionadas

--- src/pages/Consultation/Suggestions/components/SuggestionsOptions.tsx
+++ src/pages/Consultation/Suggestions/components/SuggestionsOptions.tsx
@@ -1,10 +1,18 @@
 import { IonCheckbox } from "@ionic/vue";
+import { SetupContext } from "vue";
 
-type props = {
+type Props = {
   options: Record<string, any>;
 };
 
-export const SuggestionsOptions = ({ options }: props) => {
+type Events = {
+  selectOption(value: string): void;
+};
+
+export function SuggestionsOptions(
+  { options }: Props,
+  context: SetupContext<Events>
+) {
   const renderOptionsCheckboxes = (key: string, values: Array<string>) => (
     <div key={key} class={"pl-3"}>
       <p class="font-semibold">{key}</p>
@@ -16,6 +24,7 @@ export const SuggestionsOptions = ({ options }: props) => {
             color="dark"
             class="h-full"
             value={value}
+            onIonChange={(e) => context.emit("selectOption", e.detail.value)}
           >
             {value}
           </IonCheckbox>
@@ -40,4 +49,4 @@ export const SuggestionsOptions = ({ options }: props) => {
   };
 
   return <div>{renderOptions(options)}</div>;
-};
+}
