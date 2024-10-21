99868cc48c81bd143dad690291317a58d9af0727
Leon Casas <leonn.casass@gmail.com>
2024-10-20 21:24:45 -0600

    fix

--- src/repositories/AppointmentsRepository.ts
+++ src/repositories/AppointmentsRepository.ts
@@ -2,7 +2,7 @@ import { api } from "@/services/api";
 import { Appointment } from "@/entities/Appointment";
 
 export class AppointmentsRepository {
-  public static async fetchByPatientId(id: number): Promise<Appointment[]> {
+  static async fetchByPatientId(id: number): Promise<Appointment[]> {
     return await api("GET", `/patients/${id}/appointments`);
   }
 }
--- src/repositories/authRepository.ts
+++ src/repositories/authRepository.ts
@@ -3,18 +3,18 @@ import { Nurse } from "@/entities/Nurse";
 import { AccessToken } from "@/entities/AccessToken";
 
 export class AuthRepository {
-  public static async login(credentials: {
+  static async login(credentials: {
     name: string;
     password: string;
   }): Promise<AccessToken> {
     return await api("POST", "/auth/login", credentials);
   }
 
-  public static async logout() {
+  static async logout() {
     await api("DELETE", "/auth/logout");
   }
 
-  public static async me(): Promise<Nurse> {
+  static async me(): Promise<Nurse> {
     return await api("GET", "/auth/me");
   }
 }
