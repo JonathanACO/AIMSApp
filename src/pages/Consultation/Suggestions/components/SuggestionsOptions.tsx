9c9f6c852f8ea40509a3d2df5e888daf47112fef
Leon Casas <leonn.casass@gmail.com>
2024-10-21 19:59:51 -0600

    Se agrega handler en componente de opciones de sugerencias para enviar en una string el path recursivo de donde sale la opcion seleccionada

--- src/pages/Consultation/Suggestions/components/SuggestionsOptions.tsx
+++ src/pages/Consultation/Suggestions/components/SuggestionsOptions.tsx
@@ -6,14 +6,22 @@ type Props = {
 };
 
 type Events = {
-  selectOption(value: string): void;
+  selectOption(path: string): void;
 };
 
 export function SuggestionsOptions(
   { options }: Props,
   context: SetupContext<Events>
 ) {
-  const renderOptionsCheckboxes = (key: string, values: Array<string>) => (
+  const handleSelectOption = (path: string) => {
+    context.emit("selectOption", path);
+  };
+
+  const renderOptionsCheckboxes = (
+    key: string,
+    values: Array<string>,
+    parentPath: string
+  ) => (
     <div key={key} class={"pl-3"}>
       <p class="font-semibold">{key}</p>
       <div class="grid grid-cols-3 gap-2 mb-8">
@@ -24,7 +32,9 @@ export function SuggestionsOptions(
             color="dark"
             class="h-full"
             value={value}
-            onIonChange={(e) => context.emit("selectOption", e.detail.value)}
+            onIonChange={(e) =>
+              handleSelectOption(`${parentPath}__${key}__${e.detail.value}`)
+            }
           >
             {value}
           </IonCheckbox>
@@ -33,15 +43,15 @@ export function SuggestionsOptions(
     </div>
   );
 
-  const renderOptions = (options: Record<string, any>) => {
+  const renderOptions = (options: Record<string, any>, parentPath = "") => {
     return Object.keys(options).map((key) => {
       if (Array.isArray(options[key])) {
-        return renderOptionsCheckboxes(key, options[key]);
+        return renderOptionsCheckboxes(key, options[key], parentPath);
       } else if (typeof options[key] === "object") {
         return (
           <div key={key} class={"pl-3"}>
             <p class="font-semibold">{key}</p>
-            {renderOptions(options[key])}
+            {renderOptions(options[key], `${parentPath}__${key}`)}
           </div>
         );
       }
